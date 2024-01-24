f18ec5c906a707c44b78db0e1c9634945fc55482
mynameisnttooimportant <115598682+mynameisnttooimportant@users.noreply.github.com>
2024-01-24 09:29:57 -0600

    Add files via upload

---
+++ pythonlab/CreateSomeTables.py
@@ -0,0 +1,33 @@
+import psycopg2
+
+def create_tables():
+
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="dianas",
+        user="dianas",
+        password="happy372python")
+
+    cur = conn.cursor()
+
+    sql1 = '''DROP TABLE IF EXISTS STATES;
+            CREATE TABLE STATES (
+            state text,
+            abbreviation text
+            );'''
+            
+    sql2 = '''DROP TABLE IF EXISTS STATES;
+            CREATE TABLE STATES (
+            city,
+            state text,
+            population real,
+            latitude real,
+            longitute real
+            );'''
+    
+    cur.execute(sql1)
+    cur.execute(sql2)
+    
+    
+create_tables()
