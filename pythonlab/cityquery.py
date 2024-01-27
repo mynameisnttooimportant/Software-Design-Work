41fd645c5b0af3400160b55c2e07a7b4a31b39a6
mynameisnttooimportant <115598682+mynameisnttooimportant@users.noreply.github.com>
2024-01-27 14:47:17 -0600

    Update cityquery.py

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -1,1 +1,26 @@
+import psycopg2
 
+def queries():
+
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="dianas",
+        user="dianas",
+        password="happy372python")
+
+    cur = conn.cursor()
+
+    sql1 = '''SELECT latitude, longitude FROM cities WHERE city = 'Minneapolis';'''
+    
+                
+    cur.execute(sql1)
+    row_list = cur.fetchall()
+    if row_list:
+        print(row_list)
+    else:
+        print("Northfield is not in the database")
+    
+    
+    
+queries()
