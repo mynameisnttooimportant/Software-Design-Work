246efbea3c078f33e57fae67e0262623087bf4e5
mynameisnttooimportant <115598682+mynameisnttooimportant@users.noreply.github.com>
2024-02-26 08:47:03 -0600

    Update script.js

--- csslab/static/script.js
+++ csslab/static/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const canvas = document.getElementById('drawingCanvas');
-    const drawingrules = canvas.getContext('1d');
+    const drawingrules = canvas.getContext('2d');
     let drawing = false;
 
     function startDrawing(e) {
