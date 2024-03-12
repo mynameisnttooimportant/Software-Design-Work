93796ff667d445666baa3fb6cbbf59e3ed19caac
mynameisnttooimportant <115598682+mynameisnttooimportant@users.noreply.github.com>
2024-03-12 09:48:46 -0500

    Update app.py

--- finaldirectory/app.py
+++ finaldirectory/app.py
@@ -4,7 +4,11 @@ import psycopg2
 import sys
 
 
+
+
 app = Flask(__name__, static_folder='static', template_folder='templates')
+
+
 
 
 # Define a function to establish connection to PostgreSQL
@@ -28,21 +32,39 @@ def connect_to_db():
 
 
 
+
+
+
+
+
+
+
 @app.route('/')
 def home():
     return render_template('home_page.html')
 
 
+
+
 @app.route('/search/<category>')
 def search_category(category):
 
+
     allColumns = get_all_columns(category);
     data = {
-        "criteriaOptions" : allColumns["columns"], 
+        "criteriaOptions" : allColumns["columns"],
         "criteriaOptions_dataTypes" : allColumns["columnDataTypes"]
     }
 
+
     return render_template('category_search_page.html', category=category, data=data)
+
+
+
+
+
+
+
 
 
 
@@ -55,21 +77,27 @@ def get_all_columns(db):
     conn = connect_to_db()
     cur = conn.cursor()
 
+
     sql = "SELECT column_name, data_type FROM information_schema.columns WHERE table_schema = 'public' AND table_name = %s;"
     cur.execute( sql, [db] )
     data = cur.fetchall();
 
+
     columns = []
     columnDataTypes = []
+
 
     for column in data:
         columns.append(str(column[0]))
         columnDataTypes.append(str(column[1]))
 
-    return { 
+
+    return {
         "columns" : columns,
         "columnDataTypes" : columnDataTypes
     }
+
+
 
 
 # Define a route to handle the request for character information
@@ -78,10 +106,10 @@ def fetch_category_element_names():
     request_data = request.get_json()
     category = request_data.get('fetch_from_category')
     sort_by = request_data.get('sort_by', 'name')  # Default sort criteria
-    
-    query = f"SELECT name FROM {category} ORDER BY {sort_by};"
-    
-    
+   
+    query = f"SELECT name FROM {category} ORDER BY \"{sort_by}\";"
+   
+   
     conn = connect_to_db()
     cursor = conn.cursor()
     cursor.execute(query)
@@ -89,6 +117,12 @@ def fetch_category_element_names():
     cursor.close()
     conn.close()
     return jsonify([name[0] for name in names])
+
+
+
+
+
+
 
 
 
@@ -105,6 +139,7 @@ def element_info():
         category = request_data.get('fetch_from_category')
         element = request_data.get('fetch_element')
 
+
         # Query the database
         conn = connect_to_db()
         cursor = conn.cursor()
@@ -113,14 +148,22 @@ def element_info():
         cursor.close()
         conn.close()
 
+
         if info:
             return jsonify(info)
+
 
         else:
             return jsonify({'error': 'Element not found'}), 404
 
+
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+
+
+
+
 
 
 
@@ -137,16 +180,21 @@ def check_if_filter_applies():
         element = request_data.get('fetch_element')
         criteria = request_data.get('fetch_by_criteria')
 
+
         sqlQuery = "SELECT name FROM " + category + " WHERE name = '" + element + "'"
+
 
         for criterion in criteria:
             sqlQuery += " AND "
+
 
             searchTerm_criteria = "\""+criterion.get("criteria")+"\""
             searchTerm_criteria_filter = criterion.get("criteria_filter")
             searchTerm_value = criterion.get("value").upper()
 
+
             searchQuery = ""
+
 
             #sql query depends on types of filters applied
             if(searchTerm_criteria_filter == "filter_real_is"):
@@ -164,8 +212,11 @@ def check_if_filter_applies():
             elif(searchTerm_criteria_filter == "filter_text_endsWith"):
                 searchQuery = "UPPER(" + searchTerm_criteria + ") LIKE '%" + searchTerm_value + "'"
 
+
             #adds this part of the query to full sql query
             sqlQuery += searchQuery
+
+
 
 
         # Query the database
@@ -176,14 +227,22 @@ def check_if_filter_applies():
         cursor.close()
         conn.close()
 
+
         if info:
             return jsonify({'result' : "true"})
+
 
         else:
             return jsonify({'result' : "false"})
 
+
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+
+
+
+
 
 
 
@@ -194,4 +253,4 @@ if __name__ == '__main__':
     parser.add_argument('host', help='the host server that this application runs on')
     parser.add_argument('port', help='the port that this application listens on')
     arguments = parser.parse_args()
-    app.run(host=arguments.host, port=arguments.port, debug=True) 
+    app.run(host=arguments.host, port=arguments.port, debug=True)
